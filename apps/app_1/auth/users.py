381383fe59de16ad0955561a16be4708a46874c4
Nicolás Bacquet <nibata@gmail.com>
2023-08-12 18:46:10 -0400

    Se modifica la lectura de la api para que considere los cambios realizados en el proyecto de api

--- apps/app_1/auth/users.py
+++ apps/app_1/auth/users.py
@@ -1,6 +1,7 @@
+from ..modules.encrypter_module import Encrypter
+import requests
 import jwt
 import os
-import requests
 
 
 class User:
@@ -8,19 +9,22 @@ class User:
         _jwt_key = os.environ.get("JWT_KEY")
         _jwt_algorithm = os.environ.get("JWT_ALGORITHM")
         _url_base = os.environ.get("DB_API_URL")
+        _decrypter_key = os.environ.get("CRYPTO_KEY").encode()
+        _decrypter = Encrypter(key=_decrypter_key)
+
         self._token = jwt_token
 
-        _decoded_jwt = jwt.decode(jwt=self._token, key=_jwt_key, algorithms=[_jwt_algorithm])
+        _decoded_jwt = jwt.decode(jwt=self._token, key=_jwt_key, algorithms=[_jwt_algorithm])["info"]
+        _decoded_data = _decrypter.decrypt(encrypted_text=_decoded_jwt)
 
-        self._user_mail = _decoded_jwt["user_id"]
-        self._roles = _decoded_jwt["roles"]
+        self._user_mail = _decoded_data["user_id"]
+        self._roles = _decoded_data["roles"]
 
         _url = f"{_url_base}/users/q?user_email={self._user_mail}"
         _response = requests.get(_url).json()
 
         self._is_active = _response["IsActive"]
         self._fullname = _response["FullName"]
-
 
     @property
     def is_authenticated(self) -> bool:
